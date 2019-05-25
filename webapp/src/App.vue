2193d8cdf1a9d26eec6edf917f03b25ca00211cc
Gaute Meek Olsen <gaute.meek@gmail.com>
2019-05-25 19:02:05 +0200

    header and add image button

--- webapp/src/App.vue
+++ webapp/src/App.vue
@@ -1,13 +1,37 @@
 <template>
   <div id="app">
     <nav>
-      <router-link to="/">NY KVITTERING</router-link> |
-      <router-link to="/stored">KVITTERINGER</router-link>
+      <router-link class="route" to="/">NEW RECEIPT</router-link>
+      <router-link class="route" to="/stored">ALL RECEIPTS</router-link>
     </nav>
     <router-view/>
   </div>
 </template>
 
 <style>
+@import url("https://fonts.googleapis.com/css?family=Patua+One&display=swap");
 
+body {
+  margin: 0;
+  font-family: "Patua One", cursive;
+}
+
+#app {
+  display: grid;
+  grid-template-rows: 100px 1fr;
+}
+
+nav {
+  background-image: linear-gradient(135deg, #f07d00 10%, #cf022b 100%);
+  font-size: 2em;
+  padding: 20px;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+}
+
+.route {
+  color: #333;
+  font-weight: bold;
+}
 </style>
--- webapp/src/views/NewImg.vue
+++ webapp/src/views/NewImg.vue
@@ -1,11 +1,53 @@
 <template>
-  <div class="home">
-    last opp bilde
+  <div>
+    <input type="file" v-on:change="addImg" accept="image/*" id="file">
+    <label for="file" class="btn-1">ADD</label>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'newimg'
-}
+  name: "newimg",
+  methods: {
+    addImg(e) {
+      console.log(e.target.files[0]);
+    }
+  }
+};
 </script>
+
+<style scoped>
+div {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+[type="file"] {
+  height: 0;
+  overflow: hidden;
+  width: 0;
+}
+
+[type="file"] + label {
+  border: none;
+  border-radius: 5px;
+  color: #fff;
+  cursor: pointer;
+  display: inline-block;
+  font-size: 2em;
+  font-weight: 600;
+  margin-bottom: 1rem;
+  outline: none;
+  padding: 1rem 50px;
+  position: relative;
+  vertical-align: middle;
+  background-color: #ff8100;
+  box-shadow: 0 6px #ff591a;
+}
+
+[type="file"] + label:hover {
+  box-shadow: 0 3px #ff591a;
+  top: 2px;
+}
+</style>
